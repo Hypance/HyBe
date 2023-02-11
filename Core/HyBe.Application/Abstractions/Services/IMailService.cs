efe2f25ad2f3c5828d6a87e8f63b7af6b6c19059
MemoLee <softwarewewo@gmail.com>
2023-02-12 00:08:49 +0300

    abstractions

---
+++ Core/HyBe.Application/Abstractions/Services/IMailService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Abstractions.Services
+{
+    public interface IMailService
+    {
+        
+    }
+}
