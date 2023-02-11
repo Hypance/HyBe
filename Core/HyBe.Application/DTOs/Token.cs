478e2c6b7c3b4ad936a9391d793b49f4b43c290a
MemoLee <softwarewewo@gmail.com>
2023-02-12 00:09:21 +0300

    DTOs

---
+++ Core/HyBe.Application/DTOs/Token.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.DTOs
+{
+    public class Token
+    {
+        
+    }
+}
