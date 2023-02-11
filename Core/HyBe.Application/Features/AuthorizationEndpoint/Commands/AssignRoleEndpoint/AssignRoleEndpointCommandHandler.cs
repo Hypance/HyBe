57352b04ae477dd3a8ccc0cb45cdb2341ee878c8
MemoLee <softwarewewo@gmail.com>
2023-02-12 00:13:48 +0300

    endpoints

---
+++ Core/HyBe.Application/Features/AuthorizationEndpoint/Commands/AssignRoleEndpoint/AssignRoleEndpointCommandHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.AuthorizationEndpoint.Commands.AssignRoleEndpoint
+{
+    public class AssignRoleEndpointCommandHandler
+    {
+        
+    }
+}
