dd2f57a448a41ee65d83c2e0fa38f0b280e4e552
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-12 21:18:09 +0300

    added mapping process

---
+++ Core/HyBe.Domain/Contracts/Bots/CreateBotRequest.cs
@@ -0,0 +1,24 @@
+﻿using HyBe.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Domain.Contracts.Bots
+{
+    public class CreateBotRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Market Market { get; set; }
+        public OrderType OrderType { get; set; }
+        public PositionType PositionType { get; set; }
+        public TrendStrategy TrendStrategy { get; set; }
+        public int[] AssetIds { get; set; }
+        public int[]? StrategyIds { get; set; }
+        public int[]? FormationIds { get; set; }
+        public decimal PriceVolume { get; set; }
+        public BotOptions BotOptions { get; set; }
+    }
+}
