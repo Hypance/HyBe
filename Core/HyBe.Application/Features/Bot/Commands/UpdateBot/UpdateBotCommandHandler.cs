62c1be03ba99978070d85a8e86989b35d4896cc9
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-12 21:17:19 +0300

    added processes that updates bot

---
+++ Core/HyBe.Application/Features/Bot/Commands/UpdateBot/UpdateBotCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using HyBe.Domain.Contracts.Bots;
+
+namespace HyBe.Application.Features.Bot.Commands.UpdateBot
+{
+    public class UpdateBotCommandHandler : IRequestHandler<UpdateBotCommand, IResult>
+    {
+        #region Fields
+        private readonly IBotService _botService;
+        private readonly IMapper _mapper;
+        #endregion
+
+        #region Constructor
+        public UpdateBotCommandHandler(IBotService botService, IMapper mapper)
+        {
+            _botService = botService;
+            _mapper = mapper;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IResult> Handle(UpdateBotCommand query, CancellationToken cancellationToken)
+        {
+            var botMapper = _mapper.Map<Domain.Entities.Bots.Bot>(query.Request);
+            var result = _botService.Update(botMapper);
+            if (result.Success)
+                return new SuccessResult();
+            return new ErrorResult();
+        }
+        #endregion
+    }
+}
