3d09ccb72192f8e51a35328def48567a7163d2cd
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-12 21:16:59 +0300

    added processes that creates bot

---
+++ Core/HyBe.Application/Features/Bot/Commands/CreateBot/CreateBotCommand.cs
@@ -0,0 +1,18 @@
+﻿using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+using HyBe.Domain.Contracts.Bots;
+
+
+namespace HyBe.Application.Features.Bot.Commands.CreateBot
+{
+    public class CreateBotCommand : IRequest<IResult>
+    {
+        public CreateBotRequest Request { get; set; }
+
+        public CreateBotCommand(CreateBotRequest request) 
+        {
+            Request = request;
+        }
+    }
+}
---
+++ Core/HyBe.Application/Features/Bot/Commands/CreateBot/CreateBotCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+using AutoMapper;
+using HyBe.Domain.Contracts.Bots;
+using HyBe.Domain.Entities.Bots;
+
+
+namespace HyBe.Application.Features.Bot.Commands.CreateBot
+{
+    public class CreateBotCommandHandler : IRequestHandler<CreateBotCommand, IResult>
+    {
+        #region Fields
+        private readonly IBotService _botService;
+        private readonly IMapper _mapper;
+        #endregion
+
+
+        #region Constructor
+        public CreateBotCommandHandler(IBotService botService, IMapper mapper) 
+        {
+            _botService= botService;
+            _mapper= mapper;
+        }
+        #endregion
+
+        #region Methods
+
+        public async Task<IResult> Handle(CreateBotCommand query, CancellationToken cancellationToken)
+        {
+            var botMapper = _mapper.Map<Domain.Entities.Bots.Bot>(query.Request);
+            var result = _botService.Add(botMapper);
+            if (result.Success)
+                return new SuccessResult();
+            return new ErrorResult();
+        }
+        #endregion
+    }
+}
