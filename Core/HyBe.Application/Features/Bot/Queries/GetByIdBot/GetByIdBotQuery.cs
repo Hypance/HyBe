f3ba2dd8de386da2c9b65b12e7b890fd58b3ba06
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-12 21:23:06 +0300

    added processes that gets by id bot

---
+++ Core/HyBe.Application/Features/Bot/Queries/GetByIdBot/GetByIdBotQuery.cs
@@ -0,0 +1,19 @@
+﻿using HyBe.Domain.Contracts.Bots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.Bot.Queries.GetByIdBot
+{
+    public class GetByIdBotQuery
+    {
+        public GetByIdBotRequest Request { get; set; }
+
+        public GetByIdBotQuery(GetByIdBotRequest request)
+        {
+            Request = request;
+        }
+    }
+}
