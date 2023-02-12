dca58de5fac570df646512243b8c5f1394f204e4
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-12 21:23:43 +0300

    added processes that gets by list bot

---
+++ Core/HyBe.Application/Features/Bot/Queries/GetListBot/GetListBotQuery.cs
@@ -0,0 +1,19 @@
+﻿using HyBe.Domain.Contracts.Bots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.Bot.Queries.GetListBot
+{
+    public class GetListBotQuery
+    {
+        public GetListBotRequest Request { get; set; }
+
+        public GetListBotQuery(GetListBotRequest request)
+        {
+            Request = request;
+        }
+    }
+}
---
+++ Core/HyBe.Application/Features/Bot/Queries/GetListBot/GetListBotQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.Application.Features.Backtests.Queries;
+using HyBe.Domain.Contracts.Backtests;
+using HyBe.SharedKernel.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.Bot.Queries.GetListBot
+{
+    public class GetListBotQueryHandler
+    {
+
+        #region Fileds
+        private readonly IBotService _botService;
+        private readonly IMapper _mapper;
+        #endregion
+
+        #region Constructor
+        public GetListBotQueryHandler(IBotService botService, IMapper mapper)
+        {
+            _botService = botService;
+            _mapper = mapper;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IResult> Handle(GetListBotQuery query, CancellationToken cancellationToken)
+        {
+
+            var result = _botService.GetAll();
+            if (result.Success)
+            {
+                var botMapper = _mapper.Map<List<GetListBotResponse>>(result.Data);
+                return new SuccessDataResult<List<GetListBotResponse>>(botMapper);
+            }
+            return new ErrorDataResult<List<GetListBotResponse>>();
+        }
+        #endregion
+    }
+}
