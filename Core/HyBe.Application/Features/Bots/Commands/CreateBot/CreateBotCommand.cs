6c8fd34a6b3b6d12ac180aaa316d60815a251103
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-14 23:08:41 +0300

    added processes that creates bot

--- Core/HyBe.Application/Features/Bots/Commands/CreateBot/CreateBotCommand.cs
+++ Core/HyBe.Application/Features/Bots/Commands/CreateBot/CreateBotCommand.cs
@@ -1,18 +1,16 @@
-﻿using HyBe.SharedKernel.Utilities;
-using MediatR;
-using System;
+﻿using System;
 using HyBe.Domain.Contracts.Bots;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
 
+namespace HyBe.Application.Features.Bots.Queries;
 
-namespace HyBe.Application.Features.Bots.Commands.CreateBot
+public class CreateBotCommand : IRequest<IResult>
 {
-    public class CreateBotCommand : IRequest<IResult>
-    {
-        public CreateBotRequest Request { get; set; }
+    public CreateBotRequest Request { get; set; }
 
-        public CreateBotCommand(CreateBotRequest request) 
-        {
-            Request = request;
-        }
+    public CreateBotCommand(CreateBotRequest request)
+    {
+        Request = request;
     }
 }
--- Core/HyBe.Application/Features/Bots/Commands/CreateBot/CreateBotCommandHandler.cs
+++ Core/HyBe.Application/Features/Bots/Commands/CreateBot/CreateBotCommandHandler.cs
@@ -1,39 +1,35 @@
-﻿using HyBe.Application.Abstractions.Services;
+﻿using AutoMapper;
+using HyBe.Application.Abstractions.Services;
 using HyBe.SharedKernel.Utilities;
 using MediatR;
-using System;
-using AutoMapper;
-using HyBe.Domain.Contracts.Bots;
 using HyBe.Domain.Entities;
 
-namespace HyBe.Application.Features.Bots.Commands.CreateBot
+namespace HyBe.Application.Features.Bots.Queries;
+
+public class CreateBotCommandHandler : IRequestHandler<CreateBotCommand, IResult>
 {
-    public class CreateBotCommandHandler : IRequestHandler<CreateBotCommand, IResult>
+    #region Fileds
+    private readonly IBotService _botService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public CreateBotCommandHandler(IBotService botService, IMapper mapper)
     {
-        #region Fields
-        private readonly IBotService _botService;
-        private readonly IMapper _mapper;
-        #endregion
-
-
-        #region Constructor
-        public CreateBotCommandHandler(IBotService botService, IMapper mapper) 
-        {
-            _botService= botService;
-            _mapper= mapper;
-        }
-        #endregion
-
-        #region Methods
-
-        public async Task<IResult> Handle(CreateBotCommand query, CancellationToken cancellationToken)
-        {
-            var botMapper = _mapper.Map<Bot>(query.Request);
-            var result = _botService.Add(botMapper);
-            if (result.Success)
-                return new SuccessResult();
-            return new ErrorResult();
-        }
-        #endregion
+        _botService = botService;
+        _mapper = mapper;
     }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(CreateBotCommand query, CancellationToken cancellationToken)
+    {
+        var botMapper = _mapper.Map<Bot>(query.Request);
+        var result = _botService.Add(botMapper);
+        if (result.Success)
+            return new SuccessResult();
+        return new ErrorResult(result.Message);
+    }
+    #endregion
 }
+
