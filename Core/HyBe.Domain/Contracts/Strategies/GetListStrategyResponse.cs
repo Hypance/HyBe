92d4360e02f026301c4a375fdf113a6a919d2295
Mert ATAY <merrtatay@gmail.com>
2023-02-16 22:46:13 +0300

    added process get list strategy

---
+++ Core/HyBe.Domain/Contracts/Strategies/GetListStrategyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HyBe.Domain.Enums;
+
+namespace HyBe.Domain.Contracts.Strategies;
+
+public class GetListStrategyResponse
+{
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<Signal> Signals { get; set; }
+}
+
