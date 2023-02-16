cb0e97deab8994e2ade1d20f3c2b0430561e06c1
Mert ATAY <merrtatay@gmail.com>
2023-02-16 22:47:13 +0300

    added strategy needed file

---
+++ Core/HyBe.Application/Features/Strategy/Profiles/StrategyProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Domain.Contracts.strategies;
+using HyBe.Domain.Entities.strategies;
+
+namespace HyBe.Application.Features.strategies.Profiles;
+
+public class StrategyProfile : Profile
+{
+    public StrategyProfile()
+    {
+        CreateMap<GetListstrategyResponse, strategy>();
+        CreateMap<GetByIdstrategyResponse, strategy>();
+        CreateMap<strategy, CreatestrategyRequest>();
+        CreateMap<strategy, UpdatestrategyRequest>();
+    }
+}
+
