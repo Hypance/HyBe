bcd9a9224b7bc6dc8889712e099fa9d42701aacc
Mert ATAY <merrtatay@gmail.com>
2023-02-16 22:45:50 +0300

    added process get by ıd strategy

---
+++ Core/HyBe.Application/Features/Strategy/Queries/GetByIdStrategy/GetByIdStrategyQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using HyBe.Domain.Entities.Strategys;
+using MediatR;
+using HyBe.Domain.Contracts.Strategys;
+
+namespace HyBe.Application.Features.Strategys.Queries;
+
+public class GetByIdStrategyQueryHandler : IRequestHandler<GetByIdStrategyQuery,IResult>
+{
+    #region Fileds
+    private readonly IStrategyService _strategyService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public GetByIdStrategyQueryHandler(IStrategyService strategyService, IMapper mapper)
+    {
+        _strategyService = strategyService;
+        _mapper = mapper;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(GetByIdStrategyQuery query, CancellationToken cancellationToken)
+    {
+
+        var result = _strategyService.Get(b=>b.Id == query.Request.Id);
+        if (result.Success)
+        {
+            var strategyMapper = _mapper.Map<GetByIdStrategyResponse>(result.Data);
+            return new SuccessDataResult<GetByIdStrategyResponse>(strategyMapper);
+        }
+        return new ErrorDataResult<GetByIdStrategyResponse>();
+    }
+    #endregion
+}
+
