94ddf8835d90e4e0a4d5e3ecb245fe005c8fc630
Mert ATAY <merrtatay@gmail.com>
2023-02-16 22:36:40 +0300

    added process create strategy

---
+++ Core/HyBe.Application/Features/Strategy/Commands/CreateStrategy/CreateStrategyCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HyBe.Domain.Contracts.Strategies;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+
+namespace HyBe.Application.Features.Strategies.Queries;
+
+public class CreateStrategyCommand : IRequest<IResult>
+{
+    public CreateStrategyRequest Request { get; set; }
+
+    public CreateStrategyCommand(CreateStrategyRequest request)
+    {
+        Request = request;
+    }
+}
+
