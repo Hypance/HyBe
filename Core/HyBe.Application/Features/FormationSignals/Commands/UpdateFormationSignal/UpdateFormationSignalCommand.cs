79ababa18455775530c8f1b3f3b845141646eba5
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-17 19:15:54 +0300

    added processes that updates formationsignal

---
+++ Core/HyBe.Application/Features/FormationSignals/Commands/UpdateFormationSignal/UpdateFormationSignalCommand.cs
@@ -0,0 +1,19 @@
+﻿using HyBe.Domain.Contracts.FormationSignals;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.FormationSignals.Commands.UpdateFormationSignal;
+    public class UpdateFormationSignalCommand : IRequest<IResult>
+    {
+        public UpdateFormationSignalRequest Request { get; set; }
+
+        public UpdateFormationSignalCommand(UpdateFormationSignalRequest request)
+        {
+        Request = request;
+        }
+    }
---
+++ Core/HyBe.Application/Features/FormationSignals/Commands/UpdateFormationSignal/UpdateFormationSignalCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using HyBe.Domain.Entities.Formations;
+using HyBe.Application.Features.FormationSignals.Commands.UpdateFormationSignal;
+
+namespace HyBe.Application.Features.FormationSignals.Queries;
+
+public class UpdateFormationSignalCommandHandler : IRequestHandler<UpdateFormationSignalCommand, IResult>
+{
+    #region Fields
+    private readonly IFormationSignalService _FormationSignalService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public UpdateFormationSignalCommandHandler(IFormationSignalService FormationSignalService, IMapper mapper)
+    {
+        _FormationSignalService = FormationSignalService;
+        _mapper = mapper;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(UpdateFormationSignalCommand query, CancellationToken cancellationToken)
+    {
+        var FormationSignalMapper = _mapper.Map<FormationSignal>(query.Request);
+        var result = _FormationSignalService.Update(FormationSignalMapper);
+        if (result.Success)
+            return new SuccessResult();
+        return new ErrorResult(result.Message);
+    }
+    #endregion
+}
+
