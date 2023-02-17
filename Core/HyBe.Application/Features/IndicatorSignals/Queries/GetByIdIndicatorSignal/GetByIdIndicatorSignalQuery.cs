41f1a06cac9422bb4d831fec4795291e3ccca7d2
Ruya Arabaci <ruyaarabaci2021@gmail.com>
2023-02-17 17:44:35 +0300

    added list process get by ID in Indicator Signal

---
+++ Core/HyBe.Application/Features/IndicatorSignals/Queries/GetByIdIndicatorSignal/GetByIdIndicatorSignalQuery.cs
@@ -0,0 +1,19 @@
+﻿using HyBe.Domain.Contracts.IndicatorSignal;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.IndicatorSignals.Queries.GetByIdIndicatorSignal;
+public class GetByIdIndicatorSignalQuery : IRequest<IResult>
+{
+    public GetByIdIndicatorSignalRequest Request { get; set; }
+
+    public GetByIdIndicatorSignalQuery(GetByIdIndicatorSignalRequest request)
+    {
+        Request = request;
+    }
+}
