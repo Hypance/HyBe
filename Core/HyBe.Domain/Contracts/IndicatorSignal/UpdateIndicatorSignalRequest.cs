c977f6014c9f8e2f1f7a3fccca5858885bf4f09f
Ruya Arabaci <ruyaarabaci2021@gmail.com>
2023-02-17 17:41:48 +0300

    added update process Indicator Signal

---
+++ Core/HyBe.Domain/Contracts/IndicatorSignal/UpdateIndicatorSignalRequest.cs
@@ -0,0 +1,18 @@
+﻿using HyBe.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Domain.Contracts.IndicatorSignal;
+
+public class UpdateIndicatorSignalRequest
+{
+    public string IndicatorName { get; set; }
+    public string SymbolName { get; set; }
+    public int Period { get; set; }
+    public Interval Interval { get; set; }
+    public int[] Signals { get; set; }
+    public bool IsActive { get; set; }
+}
