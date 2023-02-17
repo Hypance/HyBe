83e3e2d579e28e35bcf08507784757d158e78c11
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-17 19:15:42 +0300

    added processes that deletes formationsignal

---
+++ Core/HyBe.Application/Features/FormationSignals/Commands/DeleteFormationSignal/DeleteFormationSignalCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Application.Features.FormationSignals.Commands.DeleteFormationSignal
+{
+    public class DeleteFormationSignalCommandHandler : IRequestHandler<DeleteFormationSignalCommand, IResult>
+    {
+        #region Fields
+        private readonly IFormationSignalService _FormationSignalService;
+        private readonly IMapper _mapper;
+        #endregion
+
+        #region Constructor
+        public DeleteFormationSignalCommandHandler(IFormationSignalService FormationSignalService, IMapper mapper)
+        {
+            _FormationSignalService = FormationSignalService;
+            _mapper = mapper;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IResult> Handle(DeleteFormationSignalCommand query, CancellationToken cancellationToken)
+        {
+            var result = _FormationSignalService.Delete(query.Request.Id);
+            if (result.Success)
+                return new SuccessResult();
+            return new ErrorResult(result.Message);
+        }
+        #endregion
+    }
+}
