123481085d50c498389a61558d1bcd1c53e75f18
Ruya Arabaci <ruyaarabaci2021@gmail.com>
2023-02-17 17:38:29 +0300

    added Create Indicator Signal

---
+++ Core/HyBe.Application/Features/IndicatorSignals/Commands/CreateIndicatorSignal/CreateIndicatorSignalCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HyBe.Domain.Contracts.IndicatorSignal;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+
+namespace HyBe.Application.Features.IndicatorSignals.Commands.CreateIndicatorSignal;
+
+public class CreateIndicatorSignalCommand : IRequest<IResult>
+{
+    public CreateIndicatorSignalRequest Request { get; set; }
+
+    public CreateIndicatorSignalCommand(CreateIndicatorSignalRequest request)
+    {
+        Request = request;
+    }
+}
