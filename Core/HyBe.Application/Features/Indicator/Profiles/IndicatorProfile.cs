c1d031a14f91fa85dcd58b56a07c0d1bc2507071
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-02-22 20:58:39 +0300

    added mapping process

---
+++ Core/HyBe.Application/Features/Indicator/Profiles/IndicatorProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Domain.Contracts.Indicators;
+using HyBe.Domain.Entities.Indicators;
+
+namespace HyBe.Application.Features.Indicators.Profiles;
+
+public class IndicatorProfile : Profile
+{
+    public IndicatorProfile()
+    {
+        CreateMap<GetListIndicatorResponse, Indicator>();
+        CreateMap<GetByIdIndicatorResponse, Indicator>();
+        CreateMap<Indicator, CreateIndicatorRequest>();
+        CreateMap<Indicator, UpdateIndicatorRequest>();
+    }
+}
+
