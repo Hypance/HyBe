650db019d9481fce8da4ce581a1feb68a23b167f
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-02-22 20:56:01 +0300

    added processes that update indicator

---
+++ Core/HyBe.Application/Features/Indicator/Commands/UpdateIndicator/UpdateIndicatorCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HyBe.Domain.Contracts.Indicators;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+
+namespace HyBe.Application.Features.Indicators.Queries;
+
+public class UpdateIndicatorCommand : IRequest<IResult>
+{
+    public UpdateIndicatorRequest Request { get; set; }
+
+    public UpdateIndicatorCommand(UpdateIndicatorRequest request)
+    {
+        Request = request;
+    }
+}
