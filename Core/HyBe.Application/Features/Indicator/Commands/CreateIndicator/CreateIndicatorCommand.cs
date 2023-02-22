292fb3cec9c5775f42bbd03fefc491d85a7db96d
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-02-22 20:55:03 +0300

    added processes that creates indicator

---
+++ Core/HyBe.Application/Features/Indicator/Commands/CreateIndicator/CreateIndicatorCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HyBe.Domain.Contracts.Indicators;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+
+namespace HyBe.Application.Features.Indicators.Queries;
+
+public class CreateIndicatorCommand : IRequest<IResult>
+{
+    public CreateIndicatorRequest Request { get; set; }
+
+    public CreateIndicatorCommand(CreateIndicatorRequest request)
+    {
+        Request = request;
+    }
+}
---
+++ Core/HyBe.Application/Features/Indicator/Commands/CreateIndicator/CreateIndicatorCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using HyBe.Domain.Entities.Indicators;
+using MediatR;
+using HyBe.Domain.Contracts.Indicators;
+
+namespace HyBe.Application.Features.Indicators.Queries;
+
+public class CreateIndicatorCommandHandler : IRequestHandler<CreateIndicatorCommand, IResult>
+{
+    #region Fileds
+    private readonly IIndicatorService _indicatorService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public CreateIndicatorCommandHandler(IIndicatorService indicatorService, IMapper mapper)
+    {
+        _indicatorService = indicatorService;
+        _mapper = mapper;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(CreateIndicatorCommand query, CancellationToken cancellationToken)
+    {
+        var indicatorMapper = _mapper.Map<Indicator>(query.Request);
+        var result = _indicatorService.Add(indicatorMapper);
+        if (result.Success)
+            return new SuccessResult();
+        return new ErrorResult();
+    }
+    #endregion
+}
+
