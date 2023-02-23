b6b2c451d812c18e9095f0a9539c6e1ba97966ac
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-23 12:19:35 +0300

    added processes that creates formation

---
+++ Core/HyBe.Application/Features/Formations/Commands/CreateFormation/CreateFormationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using HyBe.Domain.Entities;
+using HyBe.Domain.Entities.Formations;
+
+namespace HyBe.Application.Features.Formations.Commands.CreateFormation;
+
+public class CreateFormationCommandHandler : IRequestHandler<CreateFormationCommand, IResult>
+{
+    private readonly IFormationService _formationService;
+    private readonly IMapper _mapper;
+    public CreateFormationCommandHandler(IFormationService formationService, IMapper mapper)
+    {
+        _formationService = formationService;
+        _mapper = mapper;
+    }
+    public async Task<IResult> Handle(CreateFormationCommand query, CancellationToken cancellationToken)
+    {
+        var formationMapper = _mapper.Map<Formation>(query.Request);
+        var result = _formationService.Add(formationMapper);
+        if (result.Success)
+            return new SuccessResult();
+        return new ErrorResult(result.Message);
+    }
+}
+
