db98cbd0a2cd91d81736459d8c51a35a346dc06c
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-24 13:06:43 +0300

    added processes that updates wallet

---
+++ Core/HyBe.Application/Features/Wallets/Commands/UpdateWallet/UpdateWalletCommand.cs
@@ -0,0 +1,17 @@
+﻿using HyBe.Domain.Contracts.Wallets;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using System;
+
+namespace HyBe.Application.Features.Wallets.Commands.UpdateWallet
+{
+    public class UpdateWalletCommand : IRequest<IResult>
+    {
+        public UpdateWalletRequest Request { get; set; }
+
+        public UpdateWalletCommand(UpdateWalletRequest request)
+        {
+            Request = request;
+        }
+    }
+}
