74fae6901348990f16711a158c2197d87f33e675
MemoLee <softwarewewo@gmail.com>
2023-02-24 17:02:16 +0300

    CustomAttributes updated

--- Core/HyBe.Application/CustomAttributes/AuthorizeDefinitionAttribute.cs
+++ Core/HyBe.Application/CustomAttributes/AuthorizeDefinitionAttribute.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace HyBe.Application.CustomAttributes
 {
-    public class AuthorizeDefinitionAttribute
+    public class AuthorizeDefinitionAttribute : Attribute
     {
-        
+        public string Menu { get; set; }
+        public string Definition { get; set; }
+        public ActionType ActionType { get; set; }
     }
 }
