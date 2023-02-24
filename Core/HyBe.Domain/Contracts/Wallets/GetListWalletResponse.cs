ee8b064ac1586ef89682cef3d068664a65752feb
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-24 13:08:07 +0300

    added mapping process

---
+++ Core/HyBe.Domain/Contracts/Wallets/GetListWalletResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyBe.Domain.Contracts.Wallets
+{
+    public class GetListWalletResponse
+    {
+        public decimal Balance { get; set; }
+    }
+}
