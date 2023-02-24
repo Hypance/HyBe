96fa6a6609436b3147598907e244daae7fbcd471
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2023-02-24 21:45:14 +0300

    Update WalletService.cs

--- Infrastructure/HyBe.Persistence/Services/WalletService.cs
+++ Infrastructure/HyBe.Persistence/Services/WalletService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using HyBe.Application.Abstractions.Services;
+using HyBe.Domain.Entities.Symbols;
+using HyBe.Persistence.Contexts;
+using HyBe.Persistence.Repositories;
+using Hypance.Core.Domain.Wallets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,10 @@ using System.Threading.Tasks;
 
 namespace HyBe.Persistence.Services
 {
-    public class WalletService
+    public class WalletService : EfRepository<Wallet>, IWalletService
     {
+        public WalletService(HypanceDbContext hypanceDbContext) : base(hypanceDbContext)
+        {
+        }
     }
 }
