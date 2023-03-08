5445ac5160b335dd259127360de9139172759175
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-03-08 23:38:06 +0300

    added servives

---
+++ Core/HyBe.Application/Abstractions/Services/IEnumService.cs
@@ -0,0 +1,10 @@
+﻿using HyBe.Domain.Entities;
+using HyBe.Domain.Entities.Enums;
+using HyBe.SharedKernel.Repositories;
+
+namespace HyBe.Application.Abstractions.Services
+{
+    public interface IEnumService : IRepository<Enum>
+    {
+    }
+}
