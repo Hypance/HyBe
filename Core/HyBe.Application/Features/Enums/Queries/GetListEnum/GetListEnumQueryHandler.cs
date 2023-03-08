814ee9e8897e46fb9cfa0bbd6a4a597e12075016
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-03-08 23:27:14 +0300

    added processes that get list enum

---
+++ Core/HyBe.Application/Features/Enums/Queries/GetListEnum/GetListEnumQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using HyBe.Domain.Entities.Enums;
+using MediatR;
+using HyBe.Domain.Contracts.Enums;
+
+namespace HyBe.Application.Features.Enums.Queries;
+
+public class GetListEnumQueryHandler : IRequestHandler<GetListEnumQuery, IResult>
+{
+    #region Fileds
+    private readonly IEnumService _enumService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public GetListEnumQueryHandler(IEnumService enumService, IMapper mapper)
+    {
+        _enumService = enumService;
+        _mapper = mapper;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(GetListEnumQuery query, CancellationToken cancellationToken)
+    {
+
+        var result = _enumService.GetAll();
+        if (result.Success)
+        {
+            var enumMapper = _mapper.Map<List<GetListEnumResponse>>(result.Data);
+            return new SuccessDataResult<List<GetListEnumResponse>>(enumMapper);
+        }
+        return new ErrorDataResult<List<GetListEnumResponse>>();
+    }
+    #endregion
+}
+
