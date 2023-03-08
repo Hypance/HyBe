46feb61181da6e342eb23434a747a318356d76bc
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-03-08 23:22:03 +0300

    added processes that update enum

---
+++ Core/HyBe.Application/Features/Enums/Commands/UpdateEnum/UpdateEnumCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HyBe.Domain.Contracts.Enums;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+
+namespace HyBe.Application.Features.Enums.Commands;
+
+public class UpdateEnumCommand : IRequest<IResult>
+{
+    public UpdateEnumRequest Request { get; set; }
+
+    public UpdateEnumCommand(UpdateEnumRequest request)
+    {
+        Request = request;
+    }
+}
---
+++ Core/HyBe.Application/Features/Enums/Commands/UpdateEnum/UpdateEnumCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AutoMapper;
+using HyBe.Application.Abstractions.Services;
+using HyBe.SharedKernel.Utilities;
+using HyBe.Domain.Entities.Enums;
+using MediatR;
+using HyBe.Domain.Contracts.Enums;
+
+namespace HyBe.Application.Features.Enums.Commands;
+
+public class UpdateEnumCommandHandler : IRequestHandler<UpdateEnumCommand, IResult>
+{
+    #region Fileds
+    private readonly IEnumService _enumService;
+    private readonly IMapper _mapper;
+    #endregion
+
+    #region Constructor
+    public UpdateEnumCommandHandler(IEnumService enumService, IMapper mapper)
+    {
+        _enumService = enumService;
+        _mapper = mapper;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<IResult> Handle(UpdateEnumCommand query, CancellationToken cancellationToken)
+    {
+        var enumMapper = _mapper.Map<Enum>(query.Request);
+        var result = _enumService.Update(enumMapper);
+        if (result.Success)
+            return new SuccessResult();
+        return new ErrorResult();
+    }
+    #endregion
+}
+
