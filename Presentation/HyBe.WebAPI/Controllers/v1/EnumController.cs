87ca69fbc62fc0266b7be56c6aee67216129415d
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2023-03-08 23:41:18 +0300

    added controller

---
+++ Presentation/HyBe.WebAPI/Controllers/v1/EnumController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using HyBe.Application.Features.Enums.Commands;
+using HyBe.Application.Features.Enums.Queries;
+using HyBe.Domain.Contracts.Enums;
+using HyBe.SharedKernel.Utilities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HyBe.WebAPI.Controllers.v1
+{
+    [Route("api/v1.0/[controller]/[action]")]
+    [ApiController]
+    public class EnumController : ControllerBase
+    {
+        #region Fileds
+        private readonly IMapper _mapper;
+        private readonly ISender _mediatr;
+        #endregion
+
+        #region Constructor
+        public EnumController(IMapper mapper, ISender mediatr)
+        {
+            _mapper = mapper;
+            _mediatr = mediatr;
+        }
+        #endregion
+
+        #region Methods
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] GetListEnumRequest request)
+        {
+            try
+            {
+                var query = _mapper.Map<GetListEnumQuery>(request);
+                var result = await _mediatr.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetByIdEnumRequest request)
+        {
+            try
+            {
+                var query = _mapper.Map<GetByIdEnumQuery>(request);
+                var result = await _mediatr.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateEnumRequest request)
+        {
+            try
+            {
+                var query = _mapper.Map<CreateEnumCommand>(request);
+                var result = await _mediatr.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateEnumRequest request)
+        {
+            try
+            {
+                var query = _mapper.Map<UpdateEnumCommand>(request);
+                var result = await _mediatr.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteEnumRequest request)
+        {
+            try
+            {
+                var query = _mapper.Map<DeleteEnumCommand>(request);
+                var result = await _mediatr.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
+
